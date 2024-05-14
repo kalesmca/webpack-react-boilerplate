68ae3ccb38997c82d6ae34fcb5327e9aafeb55a0
kaleeswaran <kalesmca@gmail.com>
2024-05-14 13:15:13 +0530

    basic design  updated

--- src/containers/layout/components/header.js
+++ src/containers/layout/components/header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "../../../index.css";
 
 const HeaderComponent = (props) =>{
     return (
         <div>
-            HeaderComponent
+           <header className="header-container">
+          <label for="check" id="">
+            <i class="fas fa-bars" id="sidebar-btn"></i>
+          </label>
+          <div className="header-left">
+            <span>
+              <h3>NMS</h3>
+            </span>
+          </div>
+          <div className="header-right">
+            <a className="logout-btn">logout</a>
+          </div>
+        </header>
         </div>
     )
 }
